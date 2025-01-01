d9126d7ca774ceb60bc7e0eea4ddec4abddd720f
AndreTrevizam <andretrevizam264@gmail.com>
2025-01-01 17:27:13 -0300

    capturando o evento de submit com os valores da despesa

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ function formatCurrencyBRL(value) {
   return value
 }
 
+// Captura o evento de submit obtendo os valores
 form.onsubmit = (event) => {
   event.preventDefault()
+
+  // Cria um objeto com os detalhes da despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    create_at: new Date()
+  }
+
+  console.log(newExpense)
 }
