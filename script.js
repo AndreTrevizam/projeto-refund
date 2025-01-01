bb72da21385e94b0527ad24c1144f14f0d9e3d10
AndreTrevizam <andretrevizam264@gmail.com>
2025-01-01 17:13:58 -0300

    formatando o input amount

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const amount = document.getElementById("amount")
 
+// Pegando o input Amount e removendo os caracteres não numéricos
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
-  console.log(value)
   
-  amount.value = value
+  // transformar o valor em centavos 
+  value = Number(value) / 100
+
+  // Atualiza o valor do input
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL"
+  })
+
+  // Retorna o valor formatado
+  return value
 }
